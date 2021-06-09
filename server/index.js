386787c0b3800d2e7ca72e68cf44ceaef24bd06f
Tomer Shvueli <tomer@shvueli.com>
2021-06-08 20:57:52 -0400

    feat: Properly read entire log file backwards via streaming

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require('express'),
 const fs = require("fs");
 
 const LOG_PATH = "/var/log";
+const BUFFER_SIZE = 1024;
 
 server.get("/:log", async (req, res) => {
   try {
@@ -17,9 +18,38 @@ server.get("/:log", async (req, res) => {
 
     const logFilePath = `${LOG_PATH}/${log}`;
 
-    fs.readFile(logFilePath, 'utf8', (err, data) => {
-      res.json(data);
-   });
+    fs.open(logFilePath, 'r', function(err, fd) {
+      if (err || !fd) {
+        return res.status(400).send({
+          message: "There was a problem opening the requested log file"
+        });
+      }
+
+      const stats = fs.fstatSync(fd);
+      const fileSize = stats.size;
+
+      let fileData = '';
+      let buffer = Buffer.alloc(BUFFER_SIZE);
+      let position = fileSize;
+      let length;
+      let eof = false;
+
+      do {
+        position = Math.max(0, position - BUFFER_SIZE);
+        length = Math.min(BUFFER_SIZE, position === 0 ? BUFFER_SIZE : position);
+
+        let bytesRead = fs.readSync(fd, buffer, {
+          length,
+          position
+        });
+
+        fileData = buffer.slice(0, bytesRead).toString() + fileData;
+
+        eof = position <= 0;
+      } while (!eof);
+
+      res.send(fileData);
+    });
   } catch (err) {
     // Be sure to log the error somewhere appropriate and notify code owners
     res.status(500).send({
